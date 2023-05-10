e28a171a4bb78be62b60d81f7740f3bc4f91a8ad
Juanse <juanseh7@hotmail.com>
2023-05-10 01:06:46 -0300

    Guarda datos a json

--- main.cpp
+++ main.cpp
@@ -3,57 +3,87 @@
 #include <nlohmann/json.hpp>
 #include <algorithm>
 #include <random>
+#include <fstream>
 
 using namespace std;
+using json = nlohmann::json;
 
 int dataConditioning(CSVData data);
-void dataConverter(int separator, string* data);
+void dataConverter(int separator, string *data);
+json saveToJsonFile(CSVData data, string path, string csvPath);
 
 int main(void)
 {
     CSVData heart;
-    
-    if(!readCSV("heart.csv", heart))
+
+    if (!readCSV("../../heart.csv", heart))
     {
         cout << "No pudimos leer heart.csv" << endl;
         return 1;
     }
 
-    //La primer columna contiene los nombres de los campos, no nos interesa conservarla.
+    // La primer columna contiene los nombres de los campos, no nos interesa conservarla.
     heart.erase(heart.begin());
 
-    
     dataConditioning(heart);
 
-    //Reordeno heart
+    // Reordeno heart
     std::random_device rd; // semilla aleatoria
     std::mt19937 g(rd());
     std::shuffle(heart.begin(), heart.end(), g);
 
+    json jsonData = saveToJsonFile(heart, "jsonData.json", "../../heart.csv");
 
     return 0;
 }
 
+json saveToJsonFile(CSVData data, string path, string csvPath)
+{
+    json jsonData = {};
+    CSVData heart;
+
+    if (!readCSV("../../heart.csv", heart))
+    {
+        cout << "No pudimos leer heart.csv" << endl;
+        return jsonData;
+    }
+
+    int j = 0;
+    for (auto it = data.begin(); it != data.end(); it++)
+    {
+        for (int i = 0; i < it[0].size(); i++)
+        {
+            jsonData[j][heart[0][i]] = it[0][i];
+        }
+        j++;
+    }
+
+    ofstream of(path);
+    of << jsonData.dump(2) << endl;
+
+    return jsonData;
+}
+
 int dataConditioning(CSVData data)
 {
-    for (auto it = data.begin(); it != data.end() ; it++)
+    for (auto it = data.begin(); it != data.end(); it++)
     {
-        //Guardo el valor de la edad como int
+        // Guardo el valor de la edad como int
         int i = 0, age = 0;
-        while(it[0][0][i] != 0)
+        while (it[0][0][i] != 0)
         {
-            age = age*10 + it[0][0][i] - '0';       
+            age = age * 10 + it[0][0][i] - '0';
             i++;
         }
         /*
-        Cambio el valor de la edad por los valores 0, 1, 2  dependiendo de si es <50 años, entre 
+        Cambio el valor de la edad por los valores 0, 1, 2  dependiendo de si es <50 años, entre
         50 y 65 años, >65 años respectivamente
         */
-        if(age < 50)
+        if (age < 50)
         {
             it[0][0] = '0';
         }
-        else if(age > 65)
+        else if (age > 65)
         {
             it[0][0] = '2';
         }
@@ -62,35 +92,35 @@ int dataConditioning(CSVData data)
             it[0][0] = '1';
         }
 
-        //Cambio el valor del nivel de azucar por 0 o 1 dependiendo de si es bajo o alto respectivamente.
+        // Cambio el valor del nivel de azucar por 0 o 1 dependiendo de si es bajo o alto respectivamente.
         dataConverter(120, &it[0][3]);
-        
-        //Cambio el valor del colesterol
+
+        // Cambio el valor del colesterol
         dataConverter(240, &it[0][4]);
 
-        //Cambio el valor de la tasa
+        // Cambio el valor de la tasa
         dataConverter(150, &it[0][7]);
-        
-        //Cambio el valor de la depresion
+
+        // Cambio el valor de la depresion
         dataConverter(1, &it[0][9]);
     }
     return 1;
 }
 
 /*************************************************************************************************************
-* Recibe un separador que contiene el valor que separa a los datos en altos y bajos ( 0 y 1 respectivamente),
-* y un puntero  a un string con el valor del respectivo atributo
-**************************************************************************************************************/
-void dataConverter(int separator, string* data)
+ * Recibe un separador que contiene el valor que separa a los datos en altos y bajos ( 0 y 1 respectivamente),
+ * y un puntero  a un string con el valor del respectivo atributo
+ **************************************************************************************************************/
+void dataConverter(int separator, string *data)
 {
     int i = 0, value = 0;
-    while(i != (*data).length() && data[0][i] != '.')
+    while (i != (*data).length() && data[0][i] != '.')
     {
-        value = value*10 + data[0][i] - '0';
+        value = value * 10 + data[0][i] - '0';
         i++;
     }
 
-    if(value < separator)
+    if (value < separator)
     {
         *data = '0';
     }
